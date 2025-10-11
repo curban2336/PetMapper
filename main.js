9f8cf3ba85efda7158fa073b4e3b8037486c1723
Chris Urban <curban841@gmail.com>
2025-10-11 15:31:56 -0400

    file adjustment and preload script addition

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 //Importing necessary modules from Electron
 const { app, BrowserWindow } = require('electron')
 
+//
+const path = require('node:path')
+
 //Function to create a new browser window
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge } = require('electron')
+
+// Expose protected methods that allow the renderer process to use
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+})
